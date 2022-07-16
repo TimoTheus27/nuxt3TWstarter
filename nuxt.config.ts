1aa720750b5f8a23666139329a1850ebd4912656
timogerth <timogerth@gmail.com>
2022-07-16 23:02:29 +0200

    setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,22 @@
 import { defineNuxtConfig } from 'nuxt'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: ['~/assets/styles/tailwind.css'],
-    build: {
-        postcss: {
-            postcssOptions: {
-                plugins: {
-                    tailwindcss: {},
-                    autoprefixer: {}
-                }
-            }
+  css: ['~/assets/styles/tailwind.css'],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {}
         }
-    },
+      }
+    }
+  },
+  vite: {
+    plugins: [
+      eslintPlugin()
+    ]
+  }
 })
