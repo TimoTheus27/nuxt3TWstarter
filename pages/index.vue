780f677b53878fe2dde2d3967f198d2999a1f7f5
timogerth <timogerth@gmail.com>
2022-07-17 13:53:47 +0200

    access route properties in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const routeProperties: string[] = [
+  `Route name: ${String(route.name)}`,
+  `Route path: ${route.path}`
+]
+</script>
+
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col justify-center align-middle items-center">
     <h1
-      class="text-5xl font-bold underline bg-emerald-300"
+      class="bg-emerald-300 p-5 text-5xl font-bold text-white"
     >
-      Hello world!
+      {{ $route.name }}-page
     </h1>
+    <p
+      v-for="(prop, i) in routeProperties"
+      :key="i"
+      class="bg-emerald-300 mt-2 text-white p-2"
+    >
+      {{ prop }}
+    </p>
+    <p
+      v-for="(param, i) in $route.query"
+      :key="i"
+      class="bg-emerald-300 mt-2 text-white p-2"
+    >
+      {{ param }}
+    </p>
   </div>
 </template>
