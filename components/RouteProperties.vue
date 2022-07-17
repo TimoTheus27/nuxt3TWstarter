20a9006f1e6dd33fd598b1f98bef550059d2c2a4
timogerth <timogerth@gmail.com>
2022-07-17 14:56:40 +0200

    Extract RouteProperties component

---
+++ components/RouteProperties.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineComponent({
+  name: 'RouteProperties'
+})
+const route = useRoute()
+
+const routeProperties: string[] = [
+  `Route name: ${String(route.name)}`,
+  `Route path: ${route.path}`
+]
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <h1
+      class="bg-emerald-300 p-5 text-5xl font-bold text-white"
+    >
+      {{ $route.name }}-page
+    </h1>
+    <p
+      v-for="(prop, i) in routeProperties"
+      :key="i"
+      class="bg-emerald-300 mt-2 text-white p-2"
+    >
+      {{ prop }}
+    </p>
+    <p
+      v-for="(param, i) in $route.query"
+      :key="i"
+      class="bg-emerald-300 mt-2 text-white p-2"
+    >
+      {{ param }}
+    </p>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,5 @@
-<script setup lang="ts">
-const route = useRoute()
-
-const routeProperties: string[] = [
-  `Route name: ${String(route.name)}`,
-  `Route path: ${route.path}`
-]
-</script>
-
 <template>
-  <div class="min-h-screen flex flex-col justify-center align-middle items-center">
-    <h1
-      class="bg-emerald-300 p-5 text-5xl font-bold text-white"
-    >
-      {{ $route.name }}-page
-    </h1>
-    <p
-      v-for="(prop, i) in routeProperties"
-      :key="i"
-      class="bg-emerald-300 mt-2 text-white p-2"
-    >
-      {{ prop }}
-    </p>
-    <p
-      v-for="(param, i) in $route.query"
-      :key="i"
-      class="bg-emerald-300 mt-2 text-white p-2"
-    >
-      {{ param }}
-    </p>
+  <div class="">
+    <RouteProperties />
   </div>
 </template>
