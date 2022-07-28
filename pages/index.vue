748b6ae3218e1c0b503895a6eb5b762a69da6116
timogerth <timogerth@gmail.com>
2022-07-28 19:38:16 +0200

    provoke client server render mismatch and show server plugin is not there on clientside

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 const apiData = await $fetch('/api/hello?test=123')
+const { $socketIOServer } = useNuxtApp()
+
+const socketServer = computed(() => {
+  return $socketIOServer || 'Unable to access'
+})
+
+if (process.server) {
+  console.log('Use socketServer on serverside: ', socketServer.value.emit('test'))
+}
+
+if (process.client) {
+  console.log('Log Clientside socketServerValue: ', socketServer.value)
+}
 </script>
 
 <template>
   <div class="">
-    <RouteProperties class="mb-1"/>
+    <RouteProperties class="mb-1" />
     <div class="text-center">
       {{ apiData }}
+    </div>
+    <div class="text-center">
+      SocketIO Server: {{ socketServer.toString() }}
     </div>
   </div>
 </template>
