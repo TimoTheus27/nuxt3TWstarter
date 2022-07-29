662d3e04f45d60d1f3d7e550cea2dece82bcb4a8
timogerth <timogerth@gmail.com>
2022-07-29 18:32:39 +0200

    cleanup index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,5 @@
 <script setup lang="ts">
 const apiData = await $fetch('/api/hello?test=123')
-const { $socketIOServer } = useNuxtApp()
-
-const socketServer = computed(() => {
-  return $socketIOServer || 'Unable to access'
-})
-
-if (process.server) {
-  console.log('Use socketServer on serverside: ', socketServer.value.emit('test'))
-}
-
-if (process.client) {
-  console.log('Log Clientside socketServerValue: ', socketServer.value)
-}
 </script>
 
 <template>
@@ -20,9 +7,6 @@ if (process.client) {
     <RouteProperties class="mb-1" />
     <div class="text-center">
       {{ apiData }}
-    </div>
-    <div class="text-center">
-      SocketIO Server: {{ socketServer.toString() }}
     </div>
   </div>
 </template>
