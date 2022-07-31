99af923b34667d164b3bbb2ca2a1f3f4ace761dd
timogerth <timogerth@gmail.com>
2022-07-31 21:23:30 +0200

    disable_server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   },
   css: ['~/assets/styles/tailwind.css'],
   modules: [
-    './modules/socket.js'
+    // './modules/socket.js'
   ],
   runtimeConfig: {
     public: {
