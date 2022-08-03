022e33901b422e279badc582a7eebe45558285f6
timogerth <timogerth@gmail.com>
2022-08-03 08:48:19 +0200

    Create teleport target fullscreen modal
    * Notice that we didn't use v-if here
      * using v-if would remove the element from the dom, hence the teleportTarget can't be registered
    * To fix this: we just hide it, with css, so that the <div id="teleportTarget" is still available when page renders

---
+++ components/TeleportTargetModal.vue
@@ -0,0 +1,25 @@
+<script setup>
+const { open } = useModal()
+</script>
+
+<template>
+  <div :class="!!open ? 'modal' : 'hidden'" class="flex items-stretch">
+    <div
+      class="grow bg-slate-400 bg-opacity-75 flex items-center justify-center"
+    >
+      <div
+        id="teleportTarget"
+        class=""
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: 999;
+}
+</style>
