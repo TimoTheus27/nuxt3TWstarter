d755789909d39f1abad793da062df1296dc795c7
timogerth <timogerth@gmail.com>
2022-08-03 08:56:36 +0200

    create teleport page & teleport page element
    * here we teleport a part of the dom into the teleportTarget
    * it uses shared state to open the modal
    * notice the :disable="open", this disables the teleport if the modal is closed
       * means the element stays on page if modal isn't open

---
+++ pages/teleport.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { open } = useModal()
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <RouteProperties class=" mb-2" />
+    <ClientOnly>
+      <Teleport :disabled="!open" to="#teleportTarget">
+        <div
+          class=" h-40 w-100 bg-emerald-300 text-center text-white text-lg p-2 flex flex-col justify-center"
+          @click="open = !open"
+        >
+          <span>I'm the element to teleport :)</span>
+          <br>
+          <span>Click me to toggle fullscreen width</span>
+        </div>
+      </Teleport>
+    </ClientOnly>
+  </div>
+</template>
